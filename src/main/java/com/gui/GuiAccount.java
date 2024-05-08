0a6e9e837b1bfcd4c6d54ccbe390ba2ec0a48fba
GustavoHDM <gustavo.h.d.marques@gmail.com>
2024-05-08 15:05:11 -0300

    error update account

--- src/main/java/com/gui/GuiAccount.java
+++ src/main/java/com/gui/GuiAccount.java
@@ -1,5 +1,6 @@
 package com.gui;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -7,8 +8,6 @@ import com.controller.AccountController;
 import com.controller.ClientController;
 import com.model.Account;
 import com.model.Client;
-import com.model.CurrentAccount;
-import com.model.SavingsAccount;
 import com.util.AccountType;
 
 public class GuiAccount {
@@ -17,12 +16,13 @@ public class GuiAccount {
 	GuiMenu mainMenu = new GuiMenu();
 	ClientController controllerClient = new ClientController();
 	AccountController controllerAccount = new AccountController();
+	List<Account> accountList;
 	Account account;
 
 	public void accountMenu() {
 
 		System.out.println("\nEscolha uma opção a baixo\n[1] Associar uma conta a um cliente\n"
-				+ "[2] Lista de Contas\n[3] ");
+				+ "[2] Lista de Contas\n[3] Alterar Informações de Conta\n[4] Excluir Conta\n[5] Voltar");
 		int opt = scan.nextInt();
 
 		switch (opt) {
@@ -31,14 +31,20 @@ public class GuiAccount {
 			accountMenu();
 			break;
 		}
-		
+
 		case 2: {
 			listing();
 			accountMenu();
 			break;
 		}
-		
+
 		case 3: {
+			update();
+			accountMenu();
+			break;
+		}
+
+		case 5: {
 			mainMenu.mainMenu();
 			break;
 		}
@@ -50,11 +56,12 @@ public class GuiAccount {
 	}
 
 	private void add() {
-		System.out.println("Selecione o tipo da conta:\n" + "[1] Conta Corrente\n" + "[2] Conta Poupanca\n" + "[3] Conta Salario\n" + "[4] Conta Pagamento\n");
+		System.out.println("Selecione o tipo da conta:\n[1] Conta Corrente\n[2] Conta Poupanca\n"
+				+ "[3] Conta Salario\n[4] Conta Pagamento\n");
 		int typeResp = scan.nextInt();
-		
-		for(AccountType type : AccountType.values()) {
-			if(type.getMenuValue() == typeResp) {
+
+		for (AccountType type : AccountType.values()) {
+			if (type.getMenuValue() == typeResp) {
 				account = type.getAccountImpl();
 				break;
 			}
@@ -70,7 +77,7 @@ public class GuiAccount {
 		account.setCpf(client.getCpf());
 		controllerAccount.create(account);
 	}
-	
+
 	private void listing() {
 		for (Map.Entry<String, Account> entry : controllerAccount.readAll().entrySet()) {
 			String cpf = entry.getKey();
@@ -84,12 +91,43 @@ public class GuiAccount {
 
 		}
 	}
-	
+
 	private void listingClient() {
 		int i = 1;
 		for (Client client : controllerClient.readAll()) {
 			System.out.println("[" + i + "]" + client.getName() + "\n");
 			i++;
 		}
+	}
+
+	private void update() {
+		listing();
+		for (Map.Entry<String, Account> entry : controllerAccount.readAll().entrySet()) {
+			accountList.add(entry.getValue());
+		}
+		System.out.println("Selecione uma conta que deseja alterar: ");
+		int resp = scan.nextInt();
+		Account account = accountList.get(resp - 1);
+		String key = account.getCpf();
+
+		System.out.println("Selecione o tipo da conta:\n[1] Conta Corrente\n[2] Conta Poupanca\n"
+				+ "[3] Conta Salario\n[4] Conta Pagamento\n");
+		int typeResp = scan.nextInt();
+		for (AccountType type : AccountType.values()) {
+			if (type.getMenuValue() == typeResp) {
+				account = type.getAccountImpl();
+				break;
+			}
+		}
+		System.out.println("n da Agencia");
+		account.setAgency(scan.nextInt());
+		System.out.print("Saldo inicial: ");
+		account.setBalance(scan.nextDouble());
+		System.out.println("Selecione um cliente para associar a conta: ");
+		listingClient();
+		int clientIndex = scan.nextInt();
+		Client client = controllerClient.readAll().get(clientIndex - 1);
+		account.setCpf(client.getCpf());
+		controllerAccount.update(account, key);
 	}
 }
